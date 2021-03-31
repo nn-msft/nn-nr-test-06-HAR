d7d5f1c4c057258ede1405626b9c6d4e4319c076
Nayana Naik <75340668+nn-msft@users.noreply.github.com>
2021-03-31 19:36:06 -0400

    Update Startup.cs
    Hello World picm to Hello world picm-01

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,7 @@ namespace NewRelicDemo_NN_01
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World picm");
+                    await context.Response.WriteAsync("Hello world picm-01");
                 });
                 endpoints.MapControllerRoute(
 
