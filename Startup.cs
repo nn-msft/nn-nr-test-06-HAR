55457a57e1dfbca214af6432647ff3a70210f2aa
Nayana Naik <75340668+nn-msft@users.noreply.github.com>
2021-04-09 13:08:23 -0400

    Update Startup.cs
    changed to Hello world oryx

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,7 @@ namespace NewRelicDemo_NN_01
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello world picm-01");
+                    await context.Response.WriteAsync("Hello world oryx");
                 });
                 endpoints.MapControllerRoute(
 
